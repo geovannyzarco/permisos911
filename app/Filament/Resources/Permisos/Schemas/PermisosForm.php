3b4c3a6e5e3085345d1a45175d4d9b4b51e80948
geovanny <geovannyzarco@gmail.com>
2025-11-21 08:16:01 -0600

    se agregado un select para buscar los empleado por oni o por nombre en el fromulario permisos.

--- app/Filament/Resources/Permisos/Schemas/PermisosForm.php
+++ app/Filament/Resources/Permisos/Schemas/PermisosForm.php
@@ -4,11 +4,13 @@ namespace App\Filament\Resources\Permisos\Schemas;
 
 use Carbon\Carbon;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Hidden;
+use Illuminate\Database\Eloquent\Model;
 
 class PermisosForm
 {
@@ -17,6 +19,15 @@ class PermisosForm
         return $schema
             ->components([
 
+                select::make('id_empleado')
+                ->label('Empleado (Nombre y ONI)')
+                ->relationship(name:'empleado',titleAttribute:'nombre')
+                ->getOptionLabelFromRecordUsing(fn (Model $record)=>"{$record->nombre} ({$record->oni})")
+                ->searchable(
+                    ['nombre','oni']
+                )
+                ->preload()
+                ->required(),
                 DatePicker::make('fecha_creacion')
                     ->label('Fecha de Creación')
                     ->default(Carbon::now())
@@ -25,9 +36,12 @@ class PermisosForm
                     ->label('Tipo de Permiso')
                     ->relationship('tipoPermiso', 'nombre')
                     ->required(),
-                DatePicker::make('desde')
-                    ->label('Desde')
-                    ->required(),
+                DateTimePicker::make('desde')
+                    ->label('Desde (Fecha y Hora)')
+                    ->required()
+                    ->seconds(false)
+                    ->displayFormat('d/m/Y H:i')
+                    ->placeholder('Seleccionar fecha y hora de inicio'),
                 DatePicker::make('hasta')
                     ->label('Hasta')
                     ->required(),
