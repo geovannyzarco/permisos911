f34dfdb864b85796d30accf9bf3f3fbf25c7ae95
geovanny <geovannyzarco@gmail.com>
2025-11-25 15:39:15 -0600

    Se ha creado el seeder para crear los usuario masivos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call(TipoPermisoSeeder::class);
         $this->call(PermisoSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(UsuariosDesdeEmpleadosSeeder::class);
 
         //$this->call(UserUnidadSeeder::class);
     }
