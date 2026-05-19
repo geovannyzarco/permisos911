e5ac733d6a855fe5fae3edd643141d6ff004c9fd
geovanny <geovannyzarco@gmail.com>
2026-05-19 10:23:26 -0600

    se agregado el codigo para que muestre los meses, horas, dias del permiso

--- app/Http/Controllers/PermisoPdfController.php
+++ app/Http/Controllers/PermisoPdfController.php
@@ -13,6 +13,30 @@ class PermisoPdfController extends Controller
         $permiso = Permiso::with(['empleado.categoria', 'empleado.unidad.division', 'tipoPermiso', 'estadoVB', 'estadoAprobado', 'jefeVb', 'jefeAprobacion'])
         ->findOrFail($id);
 
+        if ($permiso->desde && $permiso->hasta) {
+            $desde = \Carbon\Carbon::parse($permiso->desde);
+            $hasta = \Carbon\Carbon::parse($permiso->hasta);
+
+            if ($hasta->greaterThan($desde)) {
+                $diferencia = $desde->diff($hasta);
+                
+                $permiso->meses = $diferencia->m;
+                $permiso->dias = $diferencia->d;
+                $permiso->horas = $diferencia->h;
+                $permiso->minutos = $diferencia->i;
+            } else {
+                $permiso->meses = 0;
+                $permiso->dias = 0;
+                $permiso->horas = 0;
+                $permiso->minutos = 0;
+            }
+        } else {
+            $permiso->meses = 0;
+            $permiso->dias = 0;
+            $permiso->horas = 0;
+            $permiso->minutos = 0;
+        }
+
     $pdf = PDF::loadView('pdf.permiso', compact('permiso'))
         ->setPaper('letter');
 
