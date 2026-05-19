489191e94d35e8517ef132659261bf7c3357ad90
geovanny <geovannyzarco@gmail.com>
2026-05-19 11:27:04 -0600

    Se ha modificado las politicas para que los usuario con rol administracion tambien puedan editar los permisos desde el resource gestionPermisos

--- app/Policies/PermisoPolicy.php
+++ app/Policies/PermisoPolicy.php
@@ -14,57 +14,72 @@ class PermisoPolicy
     
     public function viewAny(AuthUser $authUser): bool
     {
-        return $authUser->can('ViewAny:PermisosResource');
+        return $authUser->can('ViewAny:PermisosResource')
+            || $authUser->can('ViewAny:GestionPermisoResource')
+            || $authUser->can('ViewAny:AprobacionPermisoResource');
     }
 
     public function view(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('View:PermisosResource');
+        return $authUser->can('View:PermisosResource')
+            || $authUser->can('View:GestionPermisoResource')
+            || $authUser->can('View:AprobacionPermisoResource');
     }
 
     public function create(AuthUser $authUser): bool
     {
-        return $authUser->can('Create:PermisosResource');
+        return $authUser->can('Create:PermisosResource')
+            || $authUser->can('Create:GestionPermisoResource');
     }
 
     public function update(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('Update:PermisosResource');
+        return $authUser->can('Update:PermisosResource')
+            || $authUser->can('Update:GestionPermisoResource')
+            || $authUser->can('Update:AprobacionPermisoResource');
     }
 
     public function delete(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('Delete:PermisosResource');
+        return $authUser->can('Delete:PermisosResource')
+            || $authUser->can('Delete:GestionPermisoResource')
+            || $authUser->can('Delete:AprobacionPermisoResource');
     }
 
     public function restore(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('Restore:PermisosResource');
+        return $authUser->can('Restore:PermisosResource')
+            || $authUser->can('Restore:GestionPermisoResource');
     }
 
     public function forceDelete(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('ForceDelete:PermisosResource');
+        return $authUser->can('ForceDelete:PermisosResource')
+            || $authUser->can('ForceDelete:GestionPermisoResource');
     }
 
     public function forceDeleteAny(AuthUser $authUser): bool
     {
-        return $authUser->can('ForceDeleteAny:PermisosResource');
+        return $authUser->can('ForceDeleteAny:PermisosResource')
+            || $authUser->can('ForceDeleteAny:GestionPermisoResource');
     }
 
     public function restoreAny(AuthUser $authUser): bool
     {
-        return $authUser->can('RestoreAny:PermisosResource');
+        return $authUser->can('RestoreAny:PermisosResource')
+            || $authUser->can('RestoreAny:GestionPermisoResource');
     }
 
     public function replicate(AuthUser $authUser, Permiso $permiso): bool
     {
-        return $authUser->can('Replicate:PermisosResource');
+        return $authUser->can('Replicate:PermisosResource')
+            || $authUser->can('Replicate:GestionPermisoResource');
     }
 
     public function reorder(AuthUser $authUser): bool
     {
-        return $authUser->can('Reorder:PermisosResource');
+        return $authUser->can('Reorder:PermisosResource')
+            || $authUser->can('Reorder:GestionPermisoResource');
     }
 
 }
